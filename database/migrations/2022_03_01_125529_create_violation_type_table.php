a22a6a876951ff9e556b0c965ccdbebf48ca5151
brem-lang <patrickbarcelo17@gmail.com>
2022-03-01 21:22:28 +0800

    barcelo migration

---
+++ database/migrations/2022_03_01_125529_create_violation_type_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateViolationTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('violation_type', function (Blueprint $table) {
+            $table->integer('id')->nullable()->primary();
+            $table->string('responsible_person',254)->nullable();
+            $table->date('date_apprehend')->nullable();
+            $table->string('time_apprehend',10)->nullable();
+            $table->longText('remark')->nullable();
+            $table->integer('violation_permit_id')->nullable();
+            $table->string('permit_holder',100);
+            $table->string('area_location',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('violation_type');
+    }
+}
